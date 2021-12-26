b3386251d00d71c4005c0ac79ed2fa2c092f4313
Eric Chu <ericchu@Erics-MBP.fios-router.home>
2021-12-25 21:31:56 -0500

    Day 3 Part One

--- aoc3/aoc3a/aoc3a.cpp
+++ aoc3/aoc3a/aoc3a.cpp
@@ -10,7 +10,10 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <cmath>
 using namespace std;
+
+int convert(string val);
 
 int main(int argc, const char * argv[]) {
     string line;
@@ -24,6 +27,40 @@ int main(int argc, const char * argv[]) {
     else
         cout << "No file read" << endl;
 
-    cout << "The number of bits are: " << s1.size() << endl;
+    string gamma = "";
+    string epsilon = "";
+
+    for (int i=0; i < s1[0].size(); i++){
+        int zero = 0;
+        int one = 0;
+        for (int j=0; j < s1.size(); j++){
+            if ((s1[j][i] - '0') == 0)
+                zero++;
+            if ((s1[j][i] - '0') == 1)
+                one++;
+        }
+        if (zero > one){
+            gamma += '0';
+            epsilon += '1';
+        }
+        else if (one > zero){
+            gamma += '1';
+            epsilon += '0'; 
+        }
+    }
+
+    int gammalvl = convert(gamma);
+    int epslvl = convert(epsilon);
+
+    cout << "The power consumption of the submarine is " << gammalvl * epslvl << endl;
     return 0;
+}
+
+int convert(string val){
+    int decimal = 0;
+    for (int i = 0; i < val.size(); i++){
+        if ((val[i] - '0') == 1)
+            decimal += pow(2, val.size()-1-i);
+    }
+    return decimal;
 }
